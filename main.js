12b0c7aafcd477a402c5485b42517f5c49ca2b09
SebDVS Web <69651784+sebdvsweb@users.noreply.github.com>
2025-10-15 16:58:51 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+/*
+
+Voici les manipulations de DOM à réaliser sur les div de la Grid :
+
+Div 1 : le texte change au survol, et revient sur sa valeur initiale à la sortie du over 
+
+Div 2 : la div devient un cercle au clic et redevient carré à la sortie du over
+
+Div 3 / 4 : au clic sur le bouton Mix us, la div 3 prend la place de la div 4 et vice-versa. On doit voir le changement de position se dérouler en 1s (utiliser pour cela les propriétés CSS transition et transform).
+
+Div 3 /4 : au clic sur reset, les div reprennent leur position initiale 
+
+*/
